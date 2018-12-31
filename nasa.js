cac4d7830bd46eb6b8079321a4c626aa40eb64c9
Medoboss2018 <45499460+Medoboss2018@users.noreply.github.com>
2018-12-30 16:48:54 -0800

    Update nasa.js

--- nasa.js
+++ nasa.js
@@ -179,12 +179,10 @@ client.on('message', message => {
      if (command == "kick") {
                   if(!message.channel.guild) return message.reply('** This command only for servers**');
             
-     if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.reply("**You Don't Have ` KICK_MEMBERS ` 
+     if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.reply("**You Don't Have ` KICK_MEMBERS `Permission**");
 
-Permission**");
-     if(!message.guild.member(client.user).hasPermission("KICK_MEMBERS")) return message.reply("**I Don't Have ` KICK_MEMBERS ` 
+     if(!message.guild.member(client.user).hasPermission("KICK_MEMBERS")) return message.reply("**I Don't Have ` KICK_MEMBERS ` Permission**");
 
-Permission**");
      let user = message.mentions.users.first();
      let reason = message.content.split(" ").slice(2).join(" ");
      
